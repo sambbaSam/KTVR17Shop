263c819e7f1003fd375df14308c2df6e277ceadd
pupil <pupil@ivkhk.local>
2018-09-17 09:37:06 +0300

    Добавление анотаций к классамсущностям и создание файла persistense.xml

--- src/entity/Product.java
+++ src/entity/Product.java
@@ -3,12 +3,19 @@ package entity;
 
 import java.io.Serializable;
 import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /**
  *
  * @author Sanata
  */
+@Entity
 public class Product implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private Integer price;
--- src/entity/Purchase.java
+++ src/entity/Purchase.java
@@ -3,16 +3,31 @@ package entity;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 /**
  *
- * @author pupil
+ * @author Sanata
  */
+@Entity
 public class Purchase implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @OneToOne(cascade = CascadeType.MERGE,orphanRemoval = true)
     private Product product;
+    @OneToOne(cascade = CascadeType.MERGE,orphanRemoval = true)
     private Customer customer;
+    @Temporal(TemporalType.TIMESTAMP)
     private Date saleProduct;
+    
     private Integer quantity;
 
     public Purchase() {
